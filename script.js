ecc7b180eb54de4185e9b774f255b82002153c38
MochiFox <72652250+MochiFox@users.noreply.github.com>
2020-12-12 17:12:42 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
 
 var EnemyHealth = 10;
 var EnemyDamage = 5;
-var EnemyNumber = 1;
+var EnemyNumber = 0;
 var EnemySpeed = 3;
 var Money = 200;
 var HeroExp = 0;
 var HeroMaxHealth = 100;
 var HeroHealth = 100;
 var HeroDamage = 5;
-var Time = 0;
+var ETime = 0;
+var PTime = 0;
 var paused = false;
 var EnemyMaxHealth = 10;
 var AttackLevel = 1;
+var HeroSpeed = 20;
+var idk = ''
 
-let EnemyStats = [
-    {Name:"Goblin", Health:10, Damage:5, EXP:5, Gold:5, Speed:10},
-    {Name:"Gnoblin", Health:15, Damage:16, EXP:5, Gold:5, Speed:15},
-    {Name:"Gnomlin", Health:5, Damage:5, EXP:5, Gold:5, Speed:5},
-    {Name:"Gromlin", Health:5, Damage:8, EXP:5, Gold:5, Speed:8},
-    {Name:"Gremlin", Health:7, Damage:7, EXP:7, Gold:7, Speed:7},
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("TestTxt").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "https://mochifox.github.io/Test/Enemies.json", true);
+    xhttp.send();
+  }
+
+/*function DisplayText(Txt) {
+    document.getElementById("TestTxt").innerHTML = Txt
+}
+loadDoc()
+DisplayText(idk);
+*/
+
+/*let EnemyStats = [
+    {Name:"Goblin", Health:10, Damage:5, EXP:1, Gold:5, Speed:10},
+    {Name:"Gnoblin", Health:15, Damage:16, EXP:3, Gold:10, Speed:15},
+    {Name:"Gnomlin", Health:60, Damage:20, EXP:10, Gold:25, Speed:40},
+    {Name:"Gromlin", Health:20, Damage:2, EXP:25, Gold:45, Speed:1},
+    {Name:"Gremlin", Health:80, Damage:20, EXP:30, Gold:50, Speed:10},
     {Name:"a", Health:99, Damage:9, EXP:9, Gold:9, Speed:9}
+]*/
+
+let ItemStats = [
+    {Name:"Practice Sword", Health:5, Damage:5, EXP:0, Gold:0, Speed:0.9},
+    {Name:"Wooden Shield", Health:25, Damage:1, EXP:0, Gold:0, Speed:1.1},
 ]
 
 $(document).ready(function () {
@@ -35,7 +61,7 @@ $(document).ready(function () {
         Money = units['Money'];
         EnemyDamage = units['EnemyDamage'];
         EnemySpeed = units['EnemySpeed'];
-        Time = units['Time'];
+        ETime = units['ETime'];
     }
 
     showUpdate();
@@ -44,7 +70,7 @@ $(document).ready(function () {
         //set each unit
         showUpdate();
         getUpdate();
-        HeroDamage = AttackLevel * 5;
+        HeroDamage = AttackLevel;
         window.localStorage.setItem(
             'units', JSON.stringify({
                 'EnemyHealth': EnemyHealth,
@@ -52,9 +78,11 @@ $(document).ready(function () {
                 'HeroMaxHealth': HeroMaxHealth,
                 'HeroHealth': HeroHealth,
                 'HeroDamage': HeroDamage,
+                'HeroSpeed': HeroSpeed,
                 'Money': Money,
                 'EnemyDamage': EnemyDamage,
-                'Time': Time,
+                'ETime': ETime,
+                'PTime': PTime,
                 'EnemySpeed': EnemySpeed,
                 'paused' : paused,
                 'AttackLevel': AttackLevel
@@ -67,7 +95,7 @@ function showUpdate() {
     document.getElementById("DisplayEnemyHP").innerHTML = EnemyStats[EnemyNumber].Name +" HP: " + EnemyHealth + "/" + EnemyMaxHealth;
     document.getElementById("DisplayHeroHP").innerHTML = "HP: " + HeroHealth + "/" + HeroMaxHealth;
     document.getElementById("DisplayEnemyDamage").innerHTML = "The enemy is dealing " + EnemyDamage + ' damage every ' + EnemySpeed/10 + ' seconds';
-    document.getElementById("DisplayHeroDamage").innerHTML = "Level: " + AttackLevel + " You're damage: " + HeroDamage;
+    document.getElementById("DisplayHeroDamage").innerHTML = "Level: " + AttackLevel + " You are dealing " + HeroDamage + ' damage every ' + HeroSpeed/10 + ' seconds';
     document.getElementById("DisplayGold").innerHTML = "Gold: " + Money ;
     document.getElementById("UpgradeCost").innerHTML = "Costs: " + AttackLevel * 2 ;
     if (paused == false) {
@@ -86,7 +114,8 @@ function getEnemyHP() {
         EnemyHealth = EnemyHealth - HeroDamage;
         if (EnemyHealth <= 0) {
             Money = Money + 5;
-            Time = 0;
+            ETime = 0;
+            PTime = 0;
             EnemyHealth = EnemyMaxHealth;
         }
     }
@@ -98,7 +127,8 @@ function NextEnemy() {
         if (EnemyNumber < (EnemyStats.length - 1)) {
             EnemyNumber = EnemyNumber + 1;
             InitialiseEnemy(EnemyNumber);
-            Time = 0;
+            ETime = 0;
+            PTime = 0;
         }
         else {
             EnemyNumber = EnemyStats.length - 1;
@@ -112,7 +142,8 @@ function LastEnemy() {
         if (EnemyNumber > 0) {
             EnemyNumber = EnemyNumber - 1;
             InitialiseEnemy(EnemyNumber);
-            Time = 0;
+            ETime = 0;
+            PTime = 0;
         }
         else {
             EnemyNumber = 0;
@@ -123,30 +154,47 @@ function LastEnemy() {
 
 function UpgradeAttack() {
     if (Money >= AttackLevel * 2) {
-        Money = Money - AttackLevel * 2;
+        Money = Money - AttackLevel * 25;
         AttackLevel = AttackLevel + 1
     }
 }
 function getUpdate() {
-    if (Time >= EnemySpeed) {
-        Time = 0
-        HeroHealth = HeroHealth - EnemyDamage
+    if (ETime >= EnemySpeed) {
+        ETime = 0;
+        HeroHealth = HeroHealth - EnemyDamage;
+    }
+
+    if (EnemyHealth <= 0) {
+        Money = Money + 5;
+        ETime = 0;
+        PTime = 0;
+        EnemyHealth = EnemyMaxHealth;
+    }
+
+    if (PTime >= HeroSpeed) {
+        PTime = 0;
+        EnemyHealth = EnemyHealth - HeroDamage;
     }
 
     if (paused == true) {
-        HeroHealth = HeroHealth + HeroMaxHealth/20
+        HeroHealth = HeroHealth + HeroMaxHealth/20;
     }
     
     if (HeroHealth <= 0) {
-        Time = 0
+        ETime = 0;
+        PTime = 0;
         HeroHealth = 0;
         paused = true;
     }
+
     if (HeroHealth == HeroMaxHealth) {
         paused = false;
     }
     if (paused == false) {
-        Time = Time + 1
+        ETime = ETime + 1
+    }
+    if (paused == false) {
+        PTime = PTime + 1
     }
 
 }
@@ -159,27 +207,27 @@ function InitialiseEnemy(ID) {
 }
 
 function Initialise() {
-    InitialiseEnemy()
-    EnemyNumber = 1;
+
+    EnemyNumber = 0;
     AttackLevel = 1
     Money = 200;
     HeroExp = 0;
     HeroMaxHealth = 100;
     HeroHealth = 100;
-    HeroDamage = 5;
-    Time = 0;
+    HeroDamage = 1;
+    ETime = 0;
+    PTime = 0;
     paused = false;
     EnemyMaxHealth = 10;
-   
+    HeroSpeed = 20;
     EnemyStats = [
-        {Name:"Glitch1", Health:1, Damage:1, EXP:1, Gold:1, Speed:1},
-        {Name:"Goblin", Health:10, Damage:5, EXP:5, Gold:5, Speed:10},
-        {Name:"Gnoblin", Health:15, Damage:16, EXP:5, Gold:5, Speed:15},
-        {Name:"Gnomlin", Health:5, Damage:5, EXP:5, Gold:5, Speed:5},
-        {Name:"Gromlin", Health:5, Damage:8, EXP:5, Gold:5, Speed:8},
-        {Name:"Gremlin", Health:7, Damage:7, EXP:7, Gold:7, Speed:7},
-        {Name:"a", Health:99, Damage:9, EXP:9, Gold:9, Speed:9},
-        {Name:"Glitch2", Health:1, Damage:1, EXP:1, Gold:1, Speed:1}
-    ]
+        {Name:"Goblin", Health:10, Damage:5, EXP:1, Gold:5, Speed:10},
+        {Name:"Gnoblin", Health:15, Damage:16, EXP:3, Gold:10, Speed:15},
+        {Name:"Gnomlin", Health:60, Damage:20, EXP:10, Gold:25, Speed:40},
+        {Name:"Gromlin", Health:20, Damage:2, EXP:25, Gold:45, Speed:1},
+        {Name:"Gremlin", Health:80, Damage:20, EXP:30, Gold:50, Speed:10},
+        {Name:"a", Health:99, Damage:9, EXP:9, Gold:9, Speed:9}
+    ];
+    InitialiseEnemy(EnemyNumber)
     showUpdate();
 }
